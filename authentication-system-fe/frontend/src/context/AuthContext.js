090895b26693857414657d18f2157ef223277ac4
Anish Bade <badeanish6045@gmail.com>
2024-06-18 21:39:47 +0545

    added login logout and register functions

--- authentication-system-fe/frontend/src/context/AuthContext.js
+++ authentication-system-fe/frontend/src/context/AuthContext.js
@@ -41,9 +41,48 @@ export const AuthProvider = ({ children }) => {
 
         console.log(data);
 
-        if(response.status == 200){
-            
+        if (response.status == 200) {
+            console.log('Logged In')
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access))
+            localStorage.setItem('authTokens', JSON.stringify(data))
+            history.push('/')
+        } else {
+            console.log(response.status);
+            console.log("there was a server issue");
+            alert('Someting went wrong' + response.status)
         }
+
+    }
+
+    const registerUser = async (email, username, password, password2) => {
+        const response = await fetch('http://127.0.0.1:8000/api/register', {
+            method="POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                username,
+                password,
+                password2
+            })
+        })
+
+        if (response.status == 201) {
+            history.push('/login')
+        } else {
+            console.log(response.status);
+            console.log("there was a server issue");
+            alert('Someting went wrong' + response.status)
+        }
+    }
+
+    const logoutUser = () => {
+        setAuthTokens(null);
+        setUser(null);
+        localStorage.removeItem('authTokens')
+        history.push('/login')
     }
 
 }
