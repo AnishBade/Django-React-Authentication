6fb76e0e773b7bbf930297ef7741c203ad848ac9
Anish Bade <badeanish6045@gmail.com>
2024-06-19 11:56:24 +0545

    resolve routing issue

--- authentication-system-fe/frontend/src/App.jsx
+++ authentication-system-fe/frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from "./views/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./utils/PrivateRoute";
+import Dashboard from "./views/Dashboard";
+
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Navbar />
+        <Routes>
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        </Routes>
       </AuthProvider>
 
     </Router>
--- authentication-system-fe/frontend/src/views/Dashboard.jsx
+++ authentication-system-fe/frontend/src/views/Dashboard.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Dashboard() {
   return (
     <div>
-      
+
     </div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
