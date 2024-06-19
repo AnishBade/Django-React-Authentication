316e421ac5fb9c08a0632269de19f81c373ef036
Anish Bade <badeanish6045@gmail.com>
2024-06-19 11:19:58 +0545

    add navbar

--- authentication-system-fe/frontend/src/context/AuthContext.jsx
+++ authentication-system-fe/frontend/src/context/AuthContext.jsx
@@ -1,29 +1,30 @@
 import { createContext, useState, useEffect } from 'react'
-import jwt_decode from 'jwt-decode'
-import { useHistory } from 'react-router-dom'
+
+import { jwtDecode } from 'jwt-decode'
+import { useNavigate as useHistory } from 'react-router-dom';
 
 
-create AuthContext = createContext()
+const AuthContext = createContext();
 
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
-    const [authTokens, setAuthTokens] = useState(() => {
+    const [authTokens, setAuthTokens] = useState(() =>
         localStorage.getItem('authTokens')
             ? JSON.stringify(localStorage.getItem('authTokens'))
             : null
-    })
+    );
 
 
-    const [user, setUser] = useState(() => {
+    const [user, setUser] = useState(() =>
         localStorage.getItem('authTokens')
-            ? jwt_decode(localStorage.getItem('authTokens'))
+            ? jwtDecode(localStorage.getItem('authTokens'))
             : null
-    })
+    )
 
     const [loading, setLoading] = useState(true);
 
-    const history = useHistory()
+    const history = useHistory();
 
     const loginUser = async (email, password) => {
         const response = await fetch('http://127.0.0.1:8000/api/token', {
@@ -44,7 +45,7 @@ export const AuthProvider = ({ children }) => {
         if (response.status == 200) {
             console.log('Logged In')
             setAuthTokens(data)
-            setUser(jwt_decode(data.access))
+            setUser(jwtDecode(data.access))
             localStorage.setItem('authTokens', JSON.stringify(data))
             history.push('/')
         } else {
@@ -57,7 +58,7 @@ export const AuthProvider = ({ children }) => {
 
     const registerUser = async (email, username, password, password2) => {
         const response = await fetch('http://127.0.0.1:8000/api/register', {
-            method="POST",
+            method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -98,7 +99,7 @@ export const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         if (authTokens) {
-            setUser(jwt_decode(authTokens.access))
+            setUser(jwtDecode(authTokens.access))
         }
         setLoading(false)
 
